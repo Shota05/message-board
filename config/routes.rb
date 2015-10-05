9b74535ca1ea9c3e236a7d9c71adc3c12dafb1ec
shota05 <komatsu.shota@gmail.com>
2015-10-05 00:45:49 +0000

    Gemfile update for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :messages , only: [:create]
   root 'messages#index'
+  resources :messages , except: [:index, :new]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
